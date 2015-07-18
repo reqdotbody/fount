cacd84b66ea77a00b89260150bd7b4be32c58ed8
ecastano <esteban.rotary@gmail.com>
2015-07-17 21:15:11 -0500

    Added upvote/downvote/rank buttons, formatted results

--- public/App/Results/results_controller.js
+++ public/App/Results/results_controller.js
@@ -6,7 +6,7 @@ angular.module('fount.results', [])
 
     {
     title: "Awesome JS tutorial",
-    url: "www.google.com",
+    url: "http://www.google.com",
     votes: 4,
     username: "estebanCastano",
     date: "Dec 27, 1990",
@@ -15,7 +15,7 @@ angular.module('fount.results', [])
 
      {
     title: "Awesome JS tutorial",
-    url: "www.google.com",
+    url: "http://www.google.com",
     votes: 7,
     username: "estebanCastano",
     date: "Dec 27, 1990",
@@ -24,16 +24,120 @@ angular.module('fount.results', [])
 
     {
       title: "Neat JS tutorial",
-      url: "www.apple.com",
+      url: "http://www.apple.com",
       votes: 2,
       username: "isto",
       date: "Dec 27, 1990",
       id: "test"
-    }
+    },
 
-   
+    {
+      title: "Neat JS tutorial",
+      url: "http://www.apple.com",
+      votes: 2,
+      username: "isto",
+      date: "Dec 27, 1990",
+      id: "test"
+    },
 
+    {
+      title: "Neat JS tutorial",
+      url: "http://www.apple.com",
+      votes: 2,
+      username: "isto",
+      date: "Dec 27, 1990",
+      id: "test"
+    },
+    {
+      title: "Neat JS tutorial",
+      url: "http://www.apple.com",
+      votes: 2,
+      username: "isto",
+      date: "Dec 27, 1990",
+      id: "test"
+    },
+    {
+      title: "Neat JS tutorial",
+      url: "http://www.apple.com",
+      votes: 2,
+      username: "isto",
+      date: "Dec 27, 1990",
+      id: "test"
+    },
+    {
+      title: "Neat JS tutorial",
+      url: "http://www.apple.com",
+      votes: 2,
+      username: "isto",
+      date: "Dec 27, 1990",
+      id: "test"
+    },
+    {
+      title: "Neat JS tutorial",
+      url: "http://www.apple.com",
+      votes: 2,
+      username: "isto",
+      date: "Dec 27, 1990",
+      id: "test"
+    },
+    {
+      title: "Neat JS tutorial",
+      url: "http://www.apple.com",
+      votes: 2,
+      username: "isto",
+      date: "Dec 27, 1990",
+      id: "test"
+    },
+    {
+      title: "Neat JS tutorial",
+      url: "http://www.apple.com",
+      votes: 2,
+      username: "isto",
+      date: "Dec 27, 1990",
+      id: "test"
+    },
+    {
+      title: "Neat JS tutorial",
+      url: "http://www.apple.com",
+      votes: 2,
+      username: "isto",
+      date: "Dec 27, 1990",
+      id: "test"
+    },
+    {
+      title: "Neat JS tutorial",
+      url: "http://www.apple.com",
+      votes: 2,
+      username: "isto",
+      date: "Dec 27, 1990",
+      id: "test"
+    },
+    {
+      title: "A Mithril Tutorial -- build a sample to-do app",
+      url: "http://gilbert.ghost.io/mithril-js-tutorial-1/",
+      votes: 20,
+      username: "farhana",
+      date: "Dec 27, 2015",
+      id: "test"
+    },
+    {
+      title: "Neat JS tutorial",
+      url: "http://www.apple.com",
+      votes: 2,
+      username: "isto",
+      date: "Dec 27, 1990",
+      id: "test"
+    },
+    
   ];
+
+  // Toggles the 'visited' class
+  $scope.class = "unvisited";
+  
+  $scope.changeClass = function(){
+      if ($scope.class === "unvisited")
+          $scope.class = "visited";
+  };
   
 });
 
