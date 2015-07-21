e975935ae169eb3a83850c04b5384ee0d922ad65
Isto <istobarton@gmail.com>
2015-07-21 13:16:25 -0500

    Fixes basic app functionality for ease at startup

--- public/App/Results/results_controller.js
+++ public/App/Results/results_controller.js
@@ -14,21 +14,21 @@ angular.module('fount.results', [])
     },
 
      {
-    title: "Awesome JS tutorial"
-    url: "www.google.com"
-    votes: 7
-    username: "estebanCastano"
-    date: "Dec 27, 1990"
-    id: "test"
+    title: "Awesome JS tutorial",
+    url: "www.google.com",
+    votes: 7,
+    username: "estebanCastano",
+    date: "Dec 27, 1990",
+    id: "test",
     },
 
     {
-      title: "Neat JS tutorial"
-      url: "www.apple.com"
-      votes: 2
-      username: "isto"
-      date: "Dec 27, 1990"
-      id: "test"
+      title: "Neat JS tutorial",
+      url: "www.apple.com",
+      votes: 2,
+      username: "isto",
+      date: "Dec 27, 1990",
+      id: "test",
     }
 
    
