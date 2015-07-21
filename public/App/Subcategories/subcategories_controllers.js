e8f3ca3c5b1a97e8ceabc3e9815f0ba54a061ab9
Gregg Moore <gregg74@gmail.com>
2015-07-21 14:37:03 -0500

    Minor edits

--- public/App/Subcategories/subcategories_controllers.js
+++ public/App/Subcategories/subcategories_controllers.js
@@ -12,15 +12,27 @@ angular.module('fount.subcategory', [])
     { name: "South Asian History", cat_id: 2 },
     { name: "Renaissance", cat_id: 2 },
     { name: "World War II", cat_id: 2 },
-    { name: "Russian History", cat_id: 2 }
+    { name: "Russian History", cat_id: 2 },
+    { name: "Entrepreneurship", cat_id: 3 },
+    { name: "Finance and Capital Markets", cat_id: 3 },
+    { name: "Personal Fiance", cat_id: 3 },
+    { name: "Economics 101", cat_id: 3 },
+    { name: "Trade", cat_id: 3 },
+    { name: "Theater", cat_id: 4 },
+    { name: "Writing 101", cat_id: 4 },
+    { name: "Biology", cat_id: 5 },
+    { name: "Kayaking", cat_id: 6 },
+    { name: "Mountaineering", cat_id: 6 },
+    { name: "Rock Climbing", cat_id: 6 }
   ]
 
-   var addSubcategory = function (subcategory) {
+//Method for user to add subcategory to a category
+   $scope.addSubcategory = function (subcategory) {
   	return $http({
   		method: 'POST',
-  		name: { name: name },
+  		name: { name: subcategory },
   		cat_id: { cat_id: cat_id },
-  		url: '/#/' + { Category } + '/' + { subcategory }
+  		url: '/#/' + { categories.name } + '/' + { subcategories.name }
   	})
   }
 
