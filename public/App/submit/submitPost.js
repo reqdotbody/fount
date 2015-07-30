c5a596aea676af217db64f2489c5ec2b05f1d324
colin-h <colinharman@utexas.edu>
2015-07-30 15:20:34 -0500

    posting functionality redirects properly

--- public/App/submit/submitPost.js
+++ public/App/submit/submitPost.js
@@ -2,6 +2,8 @@ angular.module('fount.submitPost', [])
 
 .controller('SubmitPostController', function($scope, $http, $state, $rootScope, CurrentCategory){
   $scope.subCategories = [];
+
+  $scope.category = CurrentCategory.category;
   $scope.subCategory = CurrentCategory.subCategory;
 
   $scope.post = {
@@ -34,23 +36,25 @@ angular.module('fount.submitPost', [])
     }
 
     $http.post('api/v1/submit', message).
-      success(function(data, status, headers, config) {
-        // this callback will be called asynchronously
-        // when the response is available
-        console.log(data);
-      }).
-      error(function(data, status, headers, config) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-        console.log("error");
-        console.log(data);
+      then(function(data, status, headers, config){
+        $state.go("index.subcategories.results",
+          { category: $scope.category,
+            subcategory: $scope.subCategory,
+          });
       });
+      // success(function(data, status, headers, config) {
+      //   // this callback will be called asynchronously
+      //   // when the response is available
+      //   console.log(data);
+      // }).
+      // error(function(data, status, headers, config) {
+      //   // called asynchronously if an error occurs
+      //   // or server returns response with an error status.
+      //   console.log("error");
+      //   console.log(data);
+      // });
 
 
-    $state.go("index.subcategories.results",
-      { category: $scope.post.subcat.parentCategory,
-        subcategory: $scope.post.subcat.subcategory
-      });
   }
 
   $scope.getSubcats();
